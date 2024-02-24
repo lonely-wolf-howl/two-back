fcde5d2936de61b1495aab09ad007c298da2e2e5
kme-developer <electruc0095@gmail.com>
2024-02-24 15:07:22 +0900

    build. 마이크로 서비스 구조 생성 및 구동 확인.

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 4001;
+  await app.listen(port);
+  console.log(`USER-SERVICE listening on port ${port}!`);
 }
 bootstrap();
